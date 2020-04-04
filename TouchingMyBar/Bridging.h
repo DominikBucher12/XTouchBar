a26f4f76e5e8ffd4532adf0c79283cb3372baf33
Dominik Bucher <dominik.bucher@showmax.com>
2020-04-04 10:28:21 +0200

    Delete bridging header copyright

--- TouchingMyBar/Bridging.h
+++ TouchingMyBar/Bridging.h
@@ -1,11 +1,3 @@
-//
-//  TouchBarPrivateApi-Bridging.h
-//  MTMR
-//
-//  Created by Anton Palgunov on 18/03/2018.
-//  Copyright © 2018 Anton Palgunov. All rights reserved.
-//
-
 #ifndef Bridging_h
 #define Bridging_h
 
