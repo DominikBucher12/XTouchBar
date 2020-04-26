fbc75ce6e21b0371701ffefd4d50afca08f27b22
Dominik Bucher <dominik.bucher@showmax.com>
2020-04-26 19:04:46 +0200

    Fix documentation and add scrollView

--- TouchingMyBar/UI/TouchBarContainer.swift
+++ TouchingMyBar/UI/TouchBarContainer.swift
@@ -11,17 +11,20 @@ import SwiftUI
 
 struct TouchBarContainer: View {
     var body: some View {
-        HStack(alignment: .center, spacing: 10) {
-            ForEach(Configuration.default.shortcuts) { shortcut in
-                Button(action: { MasterMind.perform(shortcut) }) {
-                    Image(nsImage: shortcut.icon ?? NSImage(named: "Support")!)//swiftlint:disable:this force_unwrapping
-                        .renderingMode(.original)
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: Constants.BarElementWidth.small.rawValue, height: Constants.TouchBar.height, alignment: .center)
+        ScrollView(.horizontal) {
+            HStack(alignment: .center, spacing: 10) {
+                ForEach(Configuration.default.shortcuts) { shortcut in
+                    Button(action: { MasterMind.perform(shortcut) }) {
+                        Image(nsImage: shortcut.icon ?? NSImage(named: "Support")!)//swiftlint:disable:this force_unwrapping
+                            .renderingMode(.original)
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: Constants.BarElementWidth.small.rawValue, height: Constants.TouchBar.height, alignment: .center)
+                    }
                 }
             }
         }
+        .frame(width: Constants.TouchBar.youWishMaximumAppRegion, height: Constants.TouchBar.height)
     }
 }
 struct TouchBarContainer_Previews: PreviewProvider {
